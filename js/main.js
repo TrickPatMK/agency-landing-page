2fb573409eb25af796611797fbefe7005bfff89d
Patrick Martinus <trickmartpat77@gmail.com>
2022-01-15 19:04:08 +0700

    fix: navbar on resize

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 const triggerButton = document.getElementById("trigger-btn");
-console.log(triggerButton);
 const mobileNav = document.querySelector("#mobile-nav");
 
 triggerButton.onclick = (e) => {
   e.preventDefault();
   mobileNav.classList.toggle("active");
 };
+
+window.addEventListener("resize", () => {
+  window.innerWidth > 768 ?? mobileNav.classList.remove("active");
+});
